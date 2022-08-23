e81e14d732cb61f0c7fe05ee64b10c5d5e59dbfc
123 <hm5172@naver.com>
2022-08-23 11:09:50 +0900

    fix: searchbar grid 수정

--- src/components/molecule/Searchbar.tsx
+++ src/components/molecule/Searchbar.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { HiSearch } from "react-icons/hi";
 
-
 function Searchbar(props: any) {
   return (
-    <div className="bg-[#74bb41] text-white grid grid-cols-2
-     justify-center justify-items-center justify-self-center gap-2 m-0 p-3 grid-flow-col">
-      <input className="border-none rounded-2xl w-80 p-2 
+    <div
+      className="bg-[#74bb41] grid
+     justify-center justify-items-center gap-4 m-0 p-3"
+    >
+      <input
+        className="border-none rounded-2xl w-80 p-2 
       text-base font-semibold outline-1 outline-[#cdcdcd]"
         type={props.type}
         placeholder={props.placeholder}
-        onChange={props.onChange}/>
-        
-        <HiSearch size="30px"/>
-        
+        onChange={props.onChange}
+      />
+
+      <HiSearch className="text-white p-0"size="30px" />
     </div>
   );
 }
